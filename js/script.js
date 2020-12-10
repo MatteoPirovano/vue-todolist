58aadc358373921f5c0998da62b983b35f5e2afb
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-10 17:43:05 +0100

    Quarto commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
-// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
-// Bonus:
-// - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
 var toDoList = new Vue (
   {
     el : '#root',
@@ -18,10 +14,10 @@ var toDoList = new Vue (
       addToDo : function () {
         this.list.push(this.newToDo);
         this.newToDo = '';
+      },
+      deleteToDo: function(index) {
+        this.list.splice(index, 1);
       }
-      // delete: function() {
-      //   this.list.splice(index, 1);
-      // }
     }
   }
 );
